8932d924e89381656afc7f24acb1bc6cc116c945
Danie <daniel.wojtyczka@o2.pl>
2025-12-26 15:04:08 +0100

    usunięcie zbędnego katalogu

--- 4_python_typowanie/zad_1.py
+++
@@ -1,5 +0,0 @@
-def powitanie(name: str, surname: str) -> str:
-    return f"Cześć {name} {surname}!"
-
-wynik = powitanie("Jan", "Kowalski")
-print(wynik)
--- 4_python_typowanie/zad_2.py
+++
@@ -1,5 +0,0 @@
-def mnozenie(a: int, b: int) -> int:
-    return a * b
-
-wynik = mnozenie(10, 5)
-print(wynik)
